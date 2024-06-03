fdd767721f5854e50b95d2c8a78b8916a80e8f83
kaberege <kabgnestor@gmail.com>
2024-06-03 21:51:08 +0200

    grid lorem styled

--- project1.js
+++ project1.js
@@ -4,18 +4,26 @@ const mySide = document.getElementById("mySideBar");
 const sideDelete = document.getElementById("sideRemove");
 const barIcon = document.getElementById("sideIcon");
 
-sideDelete.addEventListener("click", () => {
-
-});
-
-barIcon.addEventListener("click", () => {
-    if(mySide.className === "navlist"){
-        mySide.className = "responsiveSide";
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 992) {
+        mySide.className = "navlist";        //return to the usual class navlist
     }
 });
 
-sideDelete.addEventListener("click", ()=>{
-mySide.className = "navlist";
+window.addEventListener("scroll", () => {
+    if (mySide.className === "responsiveSide") {
+        mySide.className = "navlist";             //remove side bar on scroll
+    }
+});
+
+barIcon.addEventListener("click", () => {
+    if (mySide.className === "navlist") {
+        mySide.className = "responsiveSide";     //display side bar when bar icon is clicked
+    }
+});
+
+sideDelete.addEventListener("click", () => {
+    mySide.className = "navlist";                //remove the side bar when close icon is clicked
 });
 
 
